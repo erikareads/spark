163c78c06c01a7fda1c97dce3a4682c54d48fc09
Erika Rowland <accounts@erikarowland.com>
2023-08-09 16:39:46 -0700

    feat: Modeling more types

--- lib/spark/builder/type.ex
+++ lib/spark/builder/type.ex
@@ -2,7 +2,14 @@ defmodule Spark.Builder.Type do
   @moduledoc false
   alias __MODULE__
 
-  @opaque t :: Type.Any.t() | Type.KeywordList.t() | Type.Map.t()
+  @opaque t ::
+            Type.Any.t()
+            | Type.KeywordList.t()
+            | Type.Map.t()
+            | Type.Atom.t()
+            | Type.String.t()
+            | Type.Boolean.t()
+            | Type.Integer.t()
 
   defmodule Any do
     @opaque t :: %__MODULE__{}
@@ -17,6 +24,26 @@ defmodule Spark.Builder.Type do
   defmodule Map do
     @opaque t :: %__MODULE__{key_type: Type.t(), value_type: Type.t()}
     defstruct [:key_type, :value_type]
+  end
+
+  defmodule Atom do
+    @opaque t :: %__MODULE__{}
+    defstruct []
+  end
+
+  defmodule String do
+    @opaque t :: %__MODULE__{}
+    defstruct []
+  end
+
+  defmodule Boolean do
+    @opaque t :: %__MODULE__{}
+    defstruct []
+  end
+
+  defmodule Integer do
+    @opaque t :: %__MODULE__{kind: :integer | :positive_integer | :non_negative_integer}
+    defstruct [:kind]
   end
 
   @spec any() :: t()
@@ -36,4 +63,22 @@ defmodule Spark.Builder.Type do
   def map(key_type, value_type) do
     %Type.Map{key_type: key_type, value_type: value_type}
   end
+
+  @spec atom() :: t()
+  def atom, do: %Type.Atom{}
+
+  @spec string() :: t()
+  def string, do: %Type.String{}
+
+  @spec boolean() :: t()
+  def boolean, do: %Type.Boolean{}
+
+  @spec integer() :: t()
+  def integer, do: %Type.Integer{kind: :integer}
+
+  @spec positive_integer() :: t()
+  def positive_integer, do: %Type.Integer{kind: :positive_integer}
+
+  @spec non_negative_integer() :: t()
+  def non_negative_integer, do: %Type.Integer{kind: :non_negative_integer}
 end
